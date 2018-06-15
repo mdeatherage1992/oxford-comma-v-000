acbfe91f7868d1bb6b0be7846a23c4b23d95a9d9
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 12:32:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
 final = array[array.length - 1]
-new_array = array.slice(final)
+new_array = array.slice(array[final])
 joined = new_array.join(',')
 return joined + "and" + final
 end
