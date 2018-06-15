33553594d30b516f67bf4e10163f22b518442773
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 12:40:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   if array.legnth = 1
     return array.join(' ')
-  elsif
-    array.length = 2
-final = array[array.length - 1]
-return array.join(' and ')
+  elsif array.length = 2
+    return array.join(' and ')
+  elsif array.length > 2
+    new_array = array.first array.size - 1
+    msg = new_array.joing(',') + "and" + array[array.length - 1]
+    return msg
+  end
 end
