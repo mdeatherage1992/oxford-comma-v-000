4dc1bd1cb256fccf246ba79c8a95aec0f96cbff1
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 12:31:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  final = array[array.length - 1]
-joined = array.join(' and ')
+final = array[array.length - 1]
+new_array = array.slice(final)
+joined = new_array.join(',')
+return joined + "and" + final
 return joined
 end
